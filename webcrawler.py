8e835b0776efa51c5d87c6daae9273fbea55f327
Abhinavvisen <visenabhinav96@gmail.com>
2017-08-03 23:27:05 +0530

    Update webcrawler.py

--- webcrawler.py
+++ webcrawler.py
@@ -6,4 +6,13 @@ def trade_spider(max_pages):
     while page < max_pages:
         url = 'https://buckysroom.org/trade/search.php?page='+str(page)
         source_code = requests.get(url)
+        plain_text = source_code.text
+        soup = BeautifulSoup(plain_text)
+        for link in soup.findAll('a',{'class':'item-name'}):
+            href = link.get('href')
+            print(href)
+        page+=1
+
+
+trade_spider(1)
         
